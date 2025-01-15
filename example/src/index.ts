601f3a47c0a47f2d26995fab451e9ad5f400b4d1
jiazheng.1991 <jiazheng_91@hotmail.com>
2025-01-15 22:09:22 +0800

    update example package json

--- example/src/index.ts
+++ example/src/index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-    privateKey: process.env.ACCOUNT_PRIVATE_KEY || '',
+    privateKey: process.env.PRIVATE_KEY || '',
     rpcUrl: process.env.RPC_PROVIDER_URL || '',
     apiKey: process.env.VIRTUALS_API_KEY || '',
     virtualApiUrl: process.env.VIRTUALS_API_URL || '',
