b51451af99ca70416a8522e03d50c6b1e6595edf
JynWong <wongjieyi5594@gmail.com>
2025-01-16 13:22:11 +0800

    add token listing func

--- example/src/index.ts
+++ example/src/index.ts
@@ -14,8 +14,18 @@ const sdkClient = new SDKClient(config);
 async function main() {
     try {
         console.log('Wallet Address:', sdkClient.getAddress());
-        const txReceipt = await sdkClient.sendEther('0.01'); 
-        console.log('Transaction Receipt:', txReceipt);
+        // const txReceipt = await sdkClient.sendEther('0.01'); 
+        // console.log('Transaction Receipt:', txReceipt);
+
+        // default page number - 1, page size - 10
+        const prototypeList = await sdkClient.getPrototypeListing();
+        console.log('Fetched Prototype Token List:', prototypeList);
+        console.log('prototypeList: ', JSON.stringify(prototypeList, null, 2)); 
+
+        // default page number - 1, page size - 10
+        const sentinentListing = await sdkClient.getSentinentListing();
+        console.log('Fetched Sentinent Token List:', sentinentListing);
+        console.log('sentinentListing: ', JSON.stringify(sentinentListing, null, 2)); 
 
     } catch (error) {
         // Handle any errors
