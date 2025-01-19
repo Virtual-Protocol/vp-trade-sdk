d5266c149b84edfb1fdb5cb02965b07faada7c81
JynWong <wongjieyi5594@gmail.com>
2025-01-20 00:37:57 +0800

    add example code

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,31 +1,33 @@
 import { SDKClient } from 'vp-trade-sdk/sdkClient';
 import dotenv from 'dotenv';
+import { TransactionReceipt } from 'ethers';
 dotenv.config();
 
 const config = {
     privateKey: process.env.PRIVATE_KEY || '',
     rpcUrl: process.env.RPC_PROVIDER_URL || '',
-    apiKey: process.env.VIRTUALS_API_KEY || '',
+    apiKey: process.env.RPC_API_KEY || '',
     virtualApiUrl: process.env.VIRTUALS_API_URL || '',
-    virtualApiKey: process.env.VIRTUALS_API_KEY || ''
 };
 
 const sdkClient = new SDKClient(config);
 async function main() {
     try {
         console.log('Wallet Address:', sdkClient.getAddress());
-        // const txReceipt = await sdkClient.sendEther('0.01'); 
-        // console.log('Transaction Receipt:', txReceipt);
 
         // default page number - 1, page size - 10
         const prototypeList = await sdkClient.getPrototypeListing();
         console.log('Fetched Prototype Token List:', prototypeList);
-        console.log('prototypeList: ', JSON.stringify(prototypeList, null, 2)); 
 
         // default page number - 1, page size - 10
         const sentinentListing = await sdkClient.getSentinentListing();
         console.log('Fetched Sentinent Token List:', sentinentListing);
-        console.log('sentinentListing: ', JSON.stringify(sentinentListing, null, 2)); 
+
+        // test swap sentient
+        const GAMESentienttTokenAddress = '0x1C4CcA7C5DB003824208aDDA61Bd749e55F463a3';
+        const amount = '0.01';
+        const receipt = await sdkClient.swapInSentientTokens(GAMESentienttTokenAddress, amount);
+        console.log('Sentient txn receipt: ', receipt); 
 
     } catch (error) {
         // Handle any errors
